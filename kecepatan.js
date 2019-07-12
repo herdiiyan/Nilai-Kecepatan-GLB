f76289913f2a1b28140ed2f47479dda41c0ef46a
herdiansyah <herdiiyan8@gmail.com>
2019-07-12 19:47:05 +0700

    Menambahkan file js menghitung kecepatan

---
+++ kecepatan.js
@@ -0,0 +1,10 @@
+function hitungKecepatan(jarak, waktu) {
+    var nilaiKecepatan;
+
+    nilaiKecepatan = jarak / waktu;
+
+    return nilaiKecepatan;
+}
+
+console.log(hitungKecepatan(5, 2));
+console.log(hitungKecepatan(10, 5));
